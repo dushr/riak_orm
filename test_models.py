2e3c07f9ce3262690a4c55d7c6a08ee91e3dd169
dushyant <dushyant@dubizzle.com>
2012-05-29 14:28:56 +0400

    adding counter based on materialized class

---
+++ test_models.py
@@ -0,0 +1,47 @@
+from db.manager import RiakManager
+from db.fields import RiakField
+
+
+
+class TestDataModel(RiakModel):
+    category = RiakField()
+    site = RiakField()
+    language = RiakField()
+    datetime = RiakField()
+    item_id = RiakField(in_key=False, required=False)
+
+    key_order = ('datetime','site','category', 'language')
+    key_seperator = '#'
+
+
+    objects = RiakManager()
+
+
+class MaterializedModel(RiakModel):
+    key = RiakModel()
+    base64key = RiakModel(in_key=False, required=False)
+    value = RiakModel(in_key=False)
+
+    key_order = ('key')
+    key_seperator = ':'
+
+    objects = RiakManager()
+
+
+class NewDataMaterialized(MaterializedModel):
+
+
+class NewDataModel(RiakModel):
+    category = RiakField()
+    site = RiakField()
+    language = RiakField()
+    date = RiakField()
+    time = RiakField()
+    item_id = RiakField(in_key=False, required=False)
+
+    key_order = ('date','time','site','category', 'language')
+    key_seperator = ':'
+
+    objects = RiakManager()
+
+    materialized = NewDataMaterialized
