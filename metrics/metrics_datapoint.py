0559469635119421d538b23ccf90c14c4defde6e
natasha <natasha@dubizzle.com>
2012-05-29 14:16:15 +0400

    metrics - changed model fields for new listing

--- metrics/metrics_datapoint.py
+++ metrics/metrics_datapoint.py
@@ -4,20 +4,21 @@ from db.fields import RiakField
 
 
 
-class NewActiveListingModel(RiakModel):
+class NewActiveListing(RiakModel):
     
     '''
     This model defines the fields for new active listing datapoint
     which will be saved in riak.
     '''
-    datetime = RiakField()
+    date = RiakField()
+    time = RiakField()
     site = RiakField()
     category = RiakField()
     language = RiakField()
     object_id = RiakField(in_key=False, required=False)
     user_id = RiakField(in_key=False, required=False)
 
-    key_order = ('datetime','site','category', 'language')
-    key_seperator = '#'
+    key_order = ('date','time','site','category', 'language')
+    key_seperator = ':'
 
     objects = RiakManager()
